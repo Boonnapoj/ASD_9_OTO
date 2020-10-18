ca96b5a51b6b4bc074ea51637c66499dc175813c
Boonnapoj Wongmaneeroj <13471242@uts.edu.au>
2020-10-19 10:14:46 +1100

    Updated

--- src/main/java/uts/asd/controller/catalogueController/UpdateRestaurantServlet.java
+++ src/main/java/uts/asd/controller/catalogueController/UpdateRestaurantServlet.java
@@ -5,10 +5,23 @@
  */
 package uts.asd.controller.catalogueController;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 /**
  *
  * @author diamo
  */
-public class UpdateRestaurantServlet {
+public class UpdateRestaurantServlet extends HttpServlet{
     
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        HttpSession session = request.getSession();  
+    }
 }
